7f3888d059eb7b1a997b0d2330232f496af478b4
Gal Amouyal <gal.amouyal@ibm.com>
2020-04-21 21:43:08 +0300

    car generate model

--- index.js
+++ index.js
@@ -7,22 +7,58 @@ console.log("Objects?");
 // lp, color, type
 // drive, fuel, stop
 
-const car = {
-  lp: 8728082333,
-  color: "red",
-  tests: [2017, 2018, 2019],
-  type: { manufacturer: "BMW", version: "X5" },
-  KM: 0,
-  drive: function (km) {
+// const car = {
+//   lp: 8728082333,
+//   color: "red",
+//   tests: [2017, 2018, 2019],
+//   type: { manufacturer: "BMW", version: "X5" },
+//   KM: 0,
+//   drive: function (km) {
+//     if (typeof km !== "number") return;
+//     this.KM = this.KM + km;
+//   },
+//   test: function (year) {
+//     if (typeof year !== "number" && (year < 2000 || year > 2050)) return;
+//     this.tests.push(year);
+//   },
+// };
+
+// const car2 = {
+//   lp: 87282223,
+//   color: "black",
+//   tests: [2019],
+//   doors: 5,
+//   type: { manufacturer: "BMW", version: "X3" },
+//   drive: function (km) {
+//     if (typeof km !== "number") return;
+//     this.KM = this.KM + km;
+//   },
+//   test: function (year) {
+//     if (typeof year !== "number" && (year < 2000 || year > 2050)) return;
+//     this.tests.push(year);
+//   },
+// };
+
+function Car(lp, type, color, doors, isCarNew) {
+  this.lp = lp;
+  this.color = color;
+  this.type = type;
+  this.doors = doors;
+  this.KM = 0;
+  this.isCarNew = isCarNew;
+  this.drive = function (km) {
     if (typeof km !== "number") return;
     this.KM = this.KM + km;
-  },
-  
-};
+  };
+}
 
-const car2 = {
-  lp: 87282223,
-  color: "black",
-  tests: [2019],
-  type: { manufacturer: "BMW", version: "X3" },
-};
+const colors = ["red", "green", "blue"];
+const types = ["BMW", "Mazda", "mercds"];
+
+for (let index = 0; index < 10; index++) {
+  const lp = Math.floor(Math.random() * 9999);
+  const randomColor = colors[Math.floor(Math.random() * 3)];
+  const randomType = types[Math.floor(Math.random() * 3)];
+  const doors = Math.floor(Math.random() * 5);
+  console.log(new Car(lp, randomColor, randomType, doors, true));
+}
