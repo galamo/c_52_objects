4e3fcd92ebafb7c8e57f35e0f38d440c3a8310a1
Gal Amouyal <gal.amouyal@ibm.com>
2020-04-21 21:18:49 +0300

    drive function

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("Objects?");
+
+// object are collection of functionality and information
+// contains avariables and functions
+
+//car
+// lp, color, type
+// drive, fuel, stop
+
+const car = {
+  lp: 8728082333,
+  color: "red",
+  tests: [2017, 2018, 2019],
+  type: { manufacturer: "BMW", version: "X5" },
+  KM: 0,
+  drive: function (km) {
+    if (typeof km !== "number") return;
+    this.KM = this.KM + km;
+  },
+  
+};
+
+const car2 = {
+  lp: 87282223,
+  color: "black",
+  tests: [2019],
+  type: { manufacturer: "BMW", version: "X3" },
+};
